30c63bddafb4546a58abaa9e7b2d02c2c3fc30cc
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-03-24 21:16:30 +0100

    ENH: rmed var declaration warnings

--- stochastics.el
+++ stochastics.el
@@ -14,32 +14,32 @@
   "Simulates a Bernoulli outcome with parameter p."
   (if (> (rand) p) nil t))
 
-(defun medical-diagnosis-core (lung-cancer TB cold stomach-flu other)
+(defun medical-diagnosis-core (lc tb c sf o)
   "Generate symptoms from signs. Curtesy of the Church tutorial at "
   "http://projects.csail.mit.edu/church/wiki/Generative_Models. "
   "Translated to Emacs Lisp by me [Elvis]"
   (progn
-    (setq lung-cancer (flip lung-cancer))
-    (setq TB (flip TB))
-    (setq cold (flip cold))
-    (setq stomach-flu (flip stomach-flu))
-    (setq other (flip other))
+    (defvar lung-cancer (flip lc))
+    (defvar TB (flip tb))
+    (defvar cold (flip c))
+    (defvar stomach-flu (flip sf))
+    (defvar other (flip o))
     
-    (setq cough (or (and cold (flip 0.5))
+    (defvar cough (or (and cold (flip 0.5))
 		    (and lung-cancer (flip 0.3))
 		    (and TB (flip 0.7))
 		    (and other (flip 0.01))))
     
-    (setq fever (or (and cold (flip 0.3))
+    (defvar fever (or (and cold (flip 0.3))
 		    (and stomach-flu (flip 0.5))
 		    (and TB (flip 0.1))
 		    (and other (flip 0.01))))
     
-    (setq chest-pain (or (and lung-cancer (flip 0.5))
+    (defvar chest-pain (or (and lung-cancer (flip 0.5))
 			 (and TB (flip 0.5))
 			 (and other (flip 0.01))))
     
-    (setq shortness-of-breath (or (and lung-cancer (flip 0.5))
+    (defvar shortness-of-breath (or (and lung-cancer (flip 0.5))
 				  (and TB (flip 0.2))
 				  (and other (flip 0.01))))
 
@@ -49,16 +49,18 @@
   "Rather well."
   (message "Well patient status:")
   (progn
-    (setq status (medical-diagnosis-core 0.01 0.005 0.2 0.1 0.1))
+    (defvar status (medical-diagnosis-core 0.01 0.005 0.2 0.1 0.1))
     (message (format "Normal patient status: %s" status))
     status))
 
 (defun medical-diagnosis-sick ()
   "This guy is thoroughly ill."
   (progn
-    (setq status (medical-diagnosis-core 0.6 0.7 0.8 0.9 1))
+    (defvar status (medical-diagnosis-core 0.6 0.7 0.8 0.9 1))
     (message (format "Sick patient status: %s" status))
     status))
 
 (medical-diagnosis-normal)
 (medical-diagnosis-sick)
+
+(byte-compile-file "hanoi.el")
