fa5d048e3d02c39b9c639350502fa491bff54a79
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-03-25 09:15:55 +0100

    tug of war generative model (in python)

---
+++ tow.py
@@ -0,0 +1,27 @@
+"""
+:Synopsis: Tug-of-War generative model
+:Author: DOHMATOB Elvis Dopgima <gmdopp@gmail.com>
+
+"""
+
+import numpy as np
+from joblib import Memory
+import tempfile
+
+mem = Memory(tempfile.mkdtemp(prefix="tow"), verbose=-1)
+flip = lambda p=.5, seed=None: np.random.rand() <= p
+strength = lambda person: 10 if mem.cache(flip)(seed=person) else 5
+lazy = lambda _: flip(1. / 3)
+pull = lambda person: .5 * strength(person) if lazy(
+  person) else strength(person)
+total_pulling = lambda team: np.sum(map(pull, team))
+winner = lambda team1, team2: team2 if total_pulling(team1) < total_pulling(
+  team2) else team1
+
+print [winner(*competitors)
+       for competitors in [(("alice", "bob"), ("sue", "tom")),
+                           (("alice", "bob"), ("sue", "tom")),
+                           (("alice", "sue"), ("bob", "tom")),
+                           (("alice", "sue"), ("bob", "tom")),
+                           (("alice", "tom"), ("bob", "sue")),
+                           (("alice", "tom"), ("bob", "sue"))]]
