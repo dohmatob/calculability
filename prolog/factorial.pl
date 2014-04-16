d293ff072f83244edd75a4468e95e11d51abaf38
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-04-16 14:56:03 +0200

    rmed useless scripts

--- prolog/factorial.pl
+++
@@ -1,6 +0,0 @@
-factorial(0, 1).
-factorial(A, B) :-
-    A > 0,
-    C is A - 1,
-    factorial(C, D),
-    B is A * D.
--- prolog/love.pl
+++
@@ -1,8 +0,0 @@
-man(john) .
-woman(frida) .
-woman(nelly) .
-duck(donald) .
-loves(john , frida) .
-loves(frida , donald) .
-loves(nelly , donald) .
-loves(donald , nelly) .
